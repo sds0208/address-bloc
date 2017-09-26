263383d692b6e4117af52da097c8c46e4ca1152e
sds0208 <sarahsantacruz86@gmail.com>
2017-09-26 15:29:16 +0000

    working on view_entry_number

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
         puts "2 - Create an entry"
         puts "3 - Search for an entry"
         puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "5 - View a specific entry number"
+        puts "6 - Exit"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -36,6 +37,10 @@ class MenuController
                 read_csv
                 main_menu
             when 5
+                system "clear"
+                view_entry_number
+                main_menu 
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
@@ -77,6 +82,17 @@ class MenuController
     
     def read_csv
     end
+    
+    def view_entry_number
+        system "clear"
+        print "Entry number: "
+        n = gets.chomp
+        if n.is_a? Numeric && n <= @entries.length
+            @entries[n]
+        else
+            print "Please enter a valid number: "
+        end    
+    end    
     
     def entry_submenu(entry)
         puts "n - next entry"
